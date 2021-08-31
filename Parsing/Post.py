e129c827da62a0bda741b5a85685b733bac0a09b
Daniil-Solo <daniil.solo1723@gmail.com>
2021-08-31 20:04:37 +0300

    add class Post with all functions and comments

---
+++ Parsing/Post.py
@@ -0,0 +1,28 @@
+import bs4
+import requests
+
+
+class Post:
+    domain = "https://www.avito.ru"
+
+    def __init__(self, short_url):
+        self.short_url = short_url
+
+    def get_data(self, params: dict) -> list:
+        """
+        This function returns data of one apartments
+        It uses parameters from params
+        Connection error will be handle on class Page
+        """
+        full_url = Post.domain + self.short_url
+        request = requests.get(full_url)
+        html = request.text
+        soup = bs4.BeautifulSoup(html, "lxml")
+
+        key_storage = dict()
+        for key in params:
+            if params[key]:
+                key_storage[key] = ""
+                handler = Handler(key)
+                key_storage[key] = handler.get_data()
+        return list(key_storage.values())
