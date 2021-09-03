c2075d9815fd4fc1baa677739b24f2a71e6debdd
Daniil-Solo <daniil.solo1723@gmail.com>
2021-09-03 17:29:31 +0300

    add simple interface in terminal

---
+++ main.py
@@ -0,0 +1,23 @@
+from Parsing.AvitoParser import AvitoParser
+
+
+if __name__ == "__main__":
+    print("Добро пожаловать в Avito-analytics")
+    while True:
+        print()
+        print("Вам доступны следующие действия:")
+        print("1. Начать парсинг данных")
+
+        answer = input("Ваш ответ: ")
+        try:
+            choice = int(answer)
+        except ValueError:
+            print("Ошибка! Вам следует указать номер действия!")
+            continue
+
+        if choice == 1:
+            parser = AvitoParser()
+            parser.start()
+        else:
+            print("Ошибка! Не обнаружено действие по данному номеру!")
+            continue
